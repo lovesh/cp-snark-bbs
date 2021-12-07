6c5e59bb8a45a46a01d4ee1929abd2ab279ff377
lovesh <lovesh.bond@gmail.com>
2021-12-08 00:39:55 +0530

    Add another test involving snark proof involving 2 credentials
    Signed-off-by: lovesh <lovesh.bond@gmail.com>

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Using LegoGroth16, the cc-SNARK specified in the LegoSNARK to prove properties o
 The cc-SNARK is [here](https://github.com/lovesh/legogro16/tree/debugging)
 
 ## Examples
-1. See test `bound_check_message` where a specific signed message is proven in some range, i.e. for public `min` and `max`, `min < message < max` 
-2. See test `bound_check_messages_sum` where sum of specific signed messages is proven in some range, i.e. for public `min` and `max`, `min < sum < max`.
+1. See test `bound_check_message` where a specific signed message is proven in some range, i.e. for public `min` and `max`, `min < message < max`. This will be useful in doing proof of age in a range. 
+2. See test `bound_check_messages_sum` where sum of specific signed messages is proven in some range, i.e. for public `min` and `max`, `min < sum < max`. This can be useful in proving that the sum of income sources is in a range.
+3. See test `compare_messages_sum` where  sum of certain signed messages from the 1st signature is proven less than the sum of certain signed messages from 2nd signature. This can be useful in proving sum of liabilities < sum of assets where liabilities and assets are signed under different signatures.
--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,13 @@ struct SumBoundCheckCircuit<F: Field> {
     values: Option<[F; 4]>,
 }
 
+/// Enforce sum of `smalls` < sum of `larges`
+#[derive(Clone)]
+struct SumCompareCircuit<F: Field> {
+    smalls: Option<[F; 4]>,
+    larges: Option<[F; 4]>,
+}
+
 impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF>
     for BoundCheckCircuit<ConstraintF>
 {
@@ -97,8 +104,69 @@ impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF>
         sum.enforce_cmp(&max, Ordering::Less, true)?;
         // sum greater than or equal to min, i.e. sum >= min
         sum.enforce_cmp(&min, Ordering::Greater, true)?;
-        // let csp = cs.clone().into_inner().unwrap();
-        // println!("{} {} {}, {:?}", csp.num_constraints, csp.num_instance_variables, csp.num_witness_variables, cs.which_is_unsatisfied());
+        Ok(())
+    }
+}
+
+impl<ConstraintF: PrimeField> ConstraintSynthesizer<ConstraintF>
+    for SumCompareCircuit<ConstraintF>
+{
+    fn generate_constraints(
+        self,
+        cs: ConstraintSystemRef<ConstraintF>,
+    ) -> Result<(), SynthesisError> {
+        let mut small_sum_vars = vec![];
+        let mut small_sum = ConstraintF::zero();
+        let mut large_sum_vars = vec![];
+        let mut large_sum = ConstraintF::zero();
+
+        let smalls = match self.smalls {
+            Some(vals) => vals.map(|m| Some(m)),
+            _ => [None, None, None, None],
+        };
+
+        let larges = match self.larges {
+            Some(vals) => vals.map(|m| Some(m)),
+            _ => [None, None, None, None],
+        };
+
+        // Note: Its important to allocate witness variables that are committed so allocate variables for
+        // all in `smalls` and then all in `larges` and then variables for sum
+
+        for v in smalls {
+            let v = FpVar::new_variable(
+                cs.clone(),
+                || {
+                    let v = v.ok_or(SynthesisError::AssignmentMissing)?;
+                    small_sum += v;
+                    Ok(v)
+                },
+                AllocationMode::Witness,
+            )?;
+            small_sum_vars.push(v);
+        }
+
+        for v in larges {
+            let v = FpVar::new_variable(
+                cs.clone(),
+                || {
+                    let v = v.ok_or(SynthesisError::AssignmentMissing)?;
+                    large_sum += v;
+                    Ok(v)
+                },
+                AllocationMode::Witness,
+            )?;
+            large_sum_vars.push(v);
+        }
+
+        let small_sum = FpVar::new_variable(cs.clone(), || Ok(small_sum), AllocationMode::Witness)?;
+        small_sum.enforce_equal(&small_sum_vars.iter().sum())?;
+
+        let large_sum = FpVar::new_variable(cs.clone(), || Ok(large_sum), AllocationMode::Witness)?;
+        large_sum.enforce_equal(&large_sum_vars.iter().sum())?;
+
+        // small_sum less than large_sum, i.e. small_sum < large_sum
+        small_sum.enforce_cmp(&large_sum, Ordering::Less, false)?;
         Ok(())
     }
 }
@@ -168,7 +236,7 @@ mod tests {
     #[test]
     fn bound_check_message() {
         // Prover has a BBS+ signature and he wants to prove that one of the signed message satisfies `min < message < max`
-        // on public `min` and `max` but hiding the message.
+        // on public `min` and `max` but hiding the message. This will be useful in doing proof of age in a range
 
         let mut rng = StdRng::seed_from_u64(0u64);
         // Prover has the BBS+ signature
@@ -317,7 +385,8 @@ mod tests {
     #[test]
     fn bound_check_messages_sum() {
         // Prover has a BBS+ signature and he wants to prove that sum of certain signed messages satisfies `min <= sum <= max`
-        // on public `min` and `max` but hiding the messages and the sum.
+        // on public `min` and `max` but hiding the messages and the sum. This can be useful in proving
+        // that the sum of income sources is in a range.
 
         let mut rng = StdRng::seed_from_u64(0u64);
         // Prover has the BBS+ signature
@@ -452,6 +521,184 @@ mod tests {
         witnesses.add(PoKSignatureBBSG1Wit::new_as_witness(
             sig.clone(),
             messages
+                .clone()
+                .into_iter()
+                .enumerate()
+                .map(|t| t)
+                .collect(),
+        ));
+        witnesses.add(Witness::PedersenCommitment(committed));
+
+        let proof = ProofG1::new(&mut rng, proof_spec.clone(), witnesses.clone(), None).unwrap();
+
+        proof.verify(proof_spec, None).unwrap();
+    }
+
+    #[test]
+    fn compare_messages_sum() {
+        // Prover has 2 BBS+ signatures and he wants to prove that sum of certain signed messages from
+        // the 1st signature are less than the sum of certain signed messages from 2nd signature.
+        // This can be useful in proving sum of liabilities < sum of assets where liabilities and assets
+        // are signed under different signatures.
+
+        let mut rng = StdRng::seed_from_u64(0u64);
+
+        // 1st BBS+ signature
+        let message_count_1 = 5;
+        let (messages_1, sig_params_1, keypair_1, sig_1) = sig_setup(&mut rng, message_count_1);
+        sig_1
+            .verify(&messages_1, &keypair_1.public_key, &sig_params_1)
+            .unwrap();
+
+        // 2nd BBS+ signature
+        let message_count_2 = 10;
+        let (messages_2, sig_params_2, keypair_2, sig_2) = sig_setup(&mut rng, message_count_2);
+        sig_2
+            .verify(&messages_2, &keypair_2.public_key, &sig_params_2)
+            .unwrap();
+
+        // There are no public inputs and there is always an instance variable as 1
+        let num_instance_variables = 1;
+        // 8 witnesses (messages), 4 from each signature
+        let commit_witness_count = 8;
+
+        // Generators for the Pedersen commitment. Its important that prover does not discrete log of these wrt each other
+        let pedersen_bases =
+            get_pedersen_bases(&mut rng, num_instance_variables, commit_witness_count);
+
+        let circuit = SumCompareCircuit::<Fr> {
+            smalls: None,
+            larges: None,
+        };
+        let params = generate_random_parameters_new::<Bls12_381, _, _>(
+            circuit,
+            &pedersen_bases,
+            commit_witness_count,
+            &mut rng,
+        )
+        .unwrap();
+
+        let pvk = prepare_verifying_key(&params.vk);
+
+        // Create commitment randomness
+        let v = Fr::rand(&mut rng);
+        let link_v = Fr::rand(&mut rng);
+
+        // Messages from 1st signature
+        // Indexes of the messages
+        let s_m_ids: [usize; 4] = [0, 1, 2, 3];
+        let mut smalls = s_m_ids
+            .iter()
+            .map(|i| messages_1[*i].clone())
+            .collect::<Vec<_>>();
+        let smalls: [Fr; 4] = [
+            smalls.remove(0),
+            smalls.remove(0),
+            smalls.remove(0),
+            smalls.remove(0),
+        ];
+
+        // Messages from 2nd signature
+        // Indexes of the messages
+        let l_m_ids: [usize; 4] = [4, 6, 7, 8];
+        let mut larges = l_m_ids
+            .iter()
+            .map(|i| messages_2[*i].clone())
+            .collect::<Vec<_>>();
+        let larges: [Fr; 4] = [
+            larges.remove(0),
+            larges.remove(0),
+            larges.remove(0),
+            larges.remove(0),
+        ];
+
+        let circuit = SumCompareCircuit {
+            smalls: Some(smalls.clone()),
+            larges: Some(larges.clone()),
+        };
+
+        // Prover creates Groth16 proof
+        let proof = create_random_proof_new(circuit, v, link_v, &params, &mut rng).unwrap();
+
+        // Verifies verifies Groth16 proof
+        assert!(verify_proof(&pvk, &proof).unwrap());
+
+        // This is not done by the verifier but the prover as safety check that the commitment is correct
+        let mut wits = vec![];
+        wits.extend_from_slice(&smalls);
+        wits.extend_from_slice(&larges);
+
+        assert!(verify_commitment_new(&params.vk, &proof, &[], &wits, &v, &link_v).unwrap());
+
+        assert!(!verify_commitment_new(&params.vk, &proof, &[], &smalls, &v, &link_v).unwrap());
+
+        assert!(!verify_commitment_new(&params.vk, &proof, &[], &larges, &v, &link_v).unwrap());
+
+        // Since both prover and verifier know the public inputs, they can independently get the commitment to the witnesses
+        let commitment_to_witnesses = get_commitment_to_witnesses(&params.vk, &proof, &[]).unwrap();
+
+        // The bases and commitment opening
+        let bases = pedersen_bases.as_slice()[1..].to_vec();
+        let mut committed = wits.clone();
+        committed.push(link_v);
+
+        // Prove the equality of messages in the 2 BBS+ signatures and `commitment_to_witnesses`
+        let mut statements = Statements::new();
+        statements.add(Statement::PoKBBSSignatureG1(PoKSignatureBBSG1Stmt {
+            params: sig_params_1.clone(),
+            public_key: keypair_1.public_key.clone(),
+            revealed_messages: BTreeMap::new(),
+        }));
+        statements.add(Statement::PoKBBSSignatureG1(PoKSignatureBBSG1Stmt {
+            params: sig_params_2.clone(),
+            public_key: keypair_2.public_key.clone(),
+            revealed_messages: BTreeMap::new(),
+        }));
+        statements.add(Statement::PedersenCommitment(PedersenCommitmentStmt {
+            bases: bases.clone(),
+            commitment: commitment_to_witnesses.clone(),
+        }));
+
+        let mut meta_statements = MetaStatements::new();
+
+        // Enforce equality between the signatures' messages and commitment's opening
+        // s_m_ids are message indexes from 1st signature, i.e. statement index 0
+        for (i, m_i) in s_m_ids.iter().enumerate() {
+            meta_statements.add(MetaStatement::WitnessEquality(EqualWitnesses(
+                vec![(0, *m_i), (2, i)]
+                    .into_iter()
+                    .collect::<BTreeSet<WitnessRef>>(),
+            )));
+        }
+
+        // l_m_ids are message indexes from 2st signature, i.e. statement index 1
+        for (i, m_i) in l_m_ids.iter().enumerate() {
+            meta_statements.add(MetaStatement::WitnessEquality(EqualWitnesses(
+                vec![(1, *m_i), (2, s_m_ids.len() + i)]
+                    .into_iter()
+                    .collect::<BTreeSet<WitnessRef>>(),
+            )));
+        }
+
+        let proof_spec = ProofSpec {
+            statements: statements.clone(),
+            meta_statements: meta_statements.clone(),
+            context: None,
+        };
+
+        let mut witnesses = Witnesses::new();
+        witnesses.add(PoKSignatureBBSG1Wit::new_as_witness(
+            sig_1.clone(),
+            messages_1
+                .clone()
+                .into_iter()
+                .enumerate()
+                .map(|t| t)
+                .collect(),
+        ));
+        witnesses.add(PoKSignatureBBSG1Wit::new_as_witness(
+            sig_2.clone(),
+            messages_2
                 .clone()
                 .into_iter()
                 .enumerate()
